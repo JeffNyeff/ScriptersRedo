5ddfda80b7fee3a4903d41b062f0eeedaf8e8f84
ClassActivity01 <u16302606@tuks.co.za>
2019-10-15 16:46:23 +0200

    Capture Book Photo

--- SCRIPTERS/Migrations/201910151442023_Initial-Migration.Designer.cs
+++ SCRIPTERS/Migrations/201910151442023_Initial-Migration.Designer.cs
@@ -7,13 +7,13 @@ namespace SCRIPTERS.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class InitialModel : IMigrationMetadata
+    public sealed partial class InitialMigration : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialModel));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialMigration));
         
         string IMigrationMetadata.Id
         {
-            get { return "201910061045013_InitialModel"; }
+            get { return "201910151442023_Initial-Migration"; }
         }
         
         string IMigrationMetadata.Source
